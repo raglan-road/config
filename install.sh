283f7397403cac372e6023e75a9447a77c3e5e95
Adam J. McIntyre <adam@flipsidecrypto.com>
2021-08-17 11:05:38 -0400

    Add mac install script

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/zsh
+echo "Installing everything we can handle in a script..."
+
+# Install Homebrew via curl
+echo "Installing Homebrew"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+echo "Homebrew installed. Update!"
+brew update
+
+# Install git via Homebrew
+echo "Installing git via Homebrew"
+brew install git
+
+# Install NVM via Homebrew
+echo "Installing nvm via Homebrew"
+brew install nvm
+
+echo "Setting up the NVM path"
+if [ ! -e ~/.zshrc ]
+    then
+    echo "Creating a .zshrc in your home directory"
+    touch ~/.zshrc
+else
+    echo "zshrc exists. Appending to it."
+fi
+
+echo "Updating zshrc"
+
+echo "export NVM_DIR=~/.nvm\nsource $(brew --prefix nvm)/nvm.sh" >> ~/.zshrc
+
+source ~/.zshrc
+
+# Install node 14 via nvm
+echo "Installing node 14"
+nvm install 14
+
+# Get TypeScript up and running as a global for tsc:watch, etc.
+npm install --global typescript
+
+echo "All set! Next step: clone the repos and have fun!"
